cc90c10a74879603fa9a007e2b781ec5c5e73086
MagnusSilverdal <magnus.silverdal@umea.nti.se>
2018-11-22 10:19:02 +0100

    La till en kommentar

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,3 +1,7 @@
+/**
+ * Ett litet testprogram för att se om Git funkar
+ * @author Magnus Silverdal
+ */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Jag ska till GitHub");
